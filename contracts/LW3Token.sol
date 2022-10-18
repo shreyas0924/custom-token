927ede2d1a8b1e19f438960c5e0b10b947fd8471
shreyas0924 <shreyasp0924@gmail.com>
2022-10-18 20:55:14 +0530

    first commit

---
+++ contracts/LW3Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol"; // ERC20 standards from open zeppelin
+
+contract LW3Token is ERC20 { // We are specifying LW3Token as ERC20 Token 
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, 10 * 10 ** 18);
+    }
+}
+
